fb37e37021edbaed7fe5599a4ef908d5c91abac5
LAB_J205 <labj205@continental.edu.pe>
2025-03-28 11:38:39 -0500

    Pta

--- mayor-tres-numeros.py
+++ mayor-tres-numeros.py
@@ -1,24 +1,37 @@
 """
 Determina el mayor de tres números ingresados por el teclado
 """
-def intercambiar_valores(numero1, numero2):
-    temporal = numero1
-    numero1 = numero2
-    numero2 = temporal
-    return numero1, numero2
+class OrdenandorTresNumeros:
+    def __init__(self):
+        self.ingresar_numeros()
 
-numero1=int(input("Ingresa el primer numero : "))
-numero2=int(input("Ingresa el segundo numero: "))
-numero3=int(input("Ingresa el tercer numero : "))
+    def intercambiar_valores(self,numero1, numero2):
+        temporal = numero1
+        numero1 = numero2
+        numero2 = temporal
+        return numero1, numero2
 
-if numero1>numero2:
-   numero1, numero2 = intercambiar_valores(numero1, numero2)
+    def ingresar_numeros(self):
+        self.numero1=int(input("Ingresa el primer numero : "))
+        self.numero2=int(input("Ingresa el segundo numero: "))
+        self.numero3=int(input("Ingresa el tercer numero : "))
 
-if numero2>numero3:
-    numero2, numero3 = intercambiar_valores(numero2, numero3)
+    def ordenar_numeros(self):
+        if self.numero1>self.numero2:
+           self.numero1, self.numero2 = self.intercambiar_valores(self.numero1, self.numero2)
 
-if numero1>numero2:
-   numero1, numero2 = intercambiar_valores(numero1, numero2)
+        if self.numero2>self.numero3:
+           self.numero2, self.numero3 = self.intercambiar_valores(self.numero2, self.numero3)
 
-print(f"numero ordenados: {numero1}, {numero2}, {numero3}")
-print(f"El mayor es {numero3}")
+        if self.numero1>self.numero2:
+           self.numero1, self.numero2 = self.intercambiar_valores(self.numero1, self.numero2)
+    def imprimir_numero(self):
+        print(self.numero3)
+    def mayor_numero_es(self):
+        self.ordenar_numeros()
+        return self.numero3
+
+if __name__=="__main__":
+
+    orden = OrdenandorTresNumeros()
+    print(f"El numero mayor es: {orden.mayor_numero_es()}")
